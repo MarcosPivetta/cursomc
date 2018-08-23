81b233535880c0a62e4208b76eb0f93455dd122e
Marcos Pivetta <marcospivetta.ti@gmail.com>
2018-08-23 20:12:13 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/marcospivetta/cursomc/services/CategoriaService.java
+++ src/main/java/com/marcospivetta/cursomc/services/CategoriaService.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Service;
 
 import com.marcospivetta.cursomc.domain.Categoria;
 import com.marcospivetta.cursomc.repositories.CategoriaRepository;
+import com.marcospivetta.cursomc.services.exceptions.ObjectNotFoundException;
+
 
 @Service
 public class CategoriaService {
@@ -14,8 +16,9 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 
-	public Categoria buscar(Integer id) {
-		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
-	}
+	public Categoria buscar(Integer id) throws ObjectNotFoundException {
+		   Optional<Categoria> obj = repo.findById(id);
+		   return obj.orElseThrow(() -> new ObjectNotFoundException(
+		      "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+		}
 }
